276c0a10e2b531328d8ac5f0733ad3dd8af19310
Alejandro Rodriguez <alejandromix2@gmail.com>
2024-05-05 13:29:19 -0400

    fix

--- index.js
+++ index.js
@@ -13,21 +13,12 @@ dotenv.config();
 
 conectarDB();
 
- const dominiosPermitidos = [process.env.FRONTEND_URL];
-
- const corsOptions = {
-     origin: function(origin, callback){
-         if(dominiosPermitidos.indexOf( origin ) !== -1 ){
-             // el origen del reques esta permitido
-
-           callback(null, true)
-         } else {
-             callback(new Error('No permitido por CORS'))
-         }
-     }
- }
-
- app.use(cors(corsOptions))
+ // Enable cors
+app.use(
+    cors({
+      origin: '*',
+    }),
+  );
  
 
 
